8dae446bf00292819177134a46b7283133896d49
akiyama <ryo.akiyama112345@gmail.com>
2024-12-04 13:34:33 +0900

    Reportモデルに編集権限チェックとページネーション機能を追加
    ページネーション（25件ごと）の設定を追加
    レポートの編集権限をチェックするメソッドを追加
    作成日を取得するヘルパーメソッドを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,12 @@
 class Report < ApplicationRecord
   belongs_to :user
+  paginates_per 25
+
+  def editable?(target_user)
+    user == target_user
+  end
+
+  def created_on
+    created_at.to_date
+  end
 end

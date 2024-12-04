c90c575a14e6cffa35dad5e9fe49b7cd3d44e1de
akiyama <ryo.akiyama112345@gmail.com>
2024-12-04 13:32:20 +0900

    Userモデルにレポート関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :reports, dependent: :destroy
   paginates_per 25
 end

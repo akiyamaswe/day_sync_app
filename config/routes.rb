e4cd70b9263ebb478babc2f6f7675a2b2205f211
akiyama <ryo.akiyama112345@gmail.com>
2024-12-04 13:29:54 +0900

    reportsリソースのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'reports/index'
-  get 'reports/show'
-  get 'reports/new'
-  get 'reports/edit'
-  get 'reports/create'
-  get 'reports/update'
-  get 'reports/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'users#index'
-  resources :users, only: %i(index show)
+
+  resources :reports
 end

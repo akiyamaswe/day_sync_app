15203b14e1c2a56518f2990d45052842ecd65895
akiyama <ryo.akiyama112345@gmail.com>
2024-12-04 13:54:51 +0900

    誤ってuserのルーティングを削除してしまったため修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'users#index'
-
+  resources :users, only: %i(index show)
+  
   resources :reports
 end

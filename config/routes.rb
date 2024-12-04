1e05b6777102ef527a13fd3489c30cc166009773
akiyama <ryo.akiyama112345@gmail.com>
2024-12-04 12:19:59 +0900

    コマンドでルーティング、コントローラー、ルーティングを初期設定で生成
    rails g controller Reports index show new edit create update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/show'
+  get 'reports/new'
+  get 'reports/edit'
+  get 'reports/create'
+  get 'reports/update'
+  get 'reports/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'users#index'

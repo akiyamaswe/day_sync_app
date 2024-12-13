145849458730cc9816e1e0303b155b89ce398ce4
akiyama <ryo.akiyama112345@gmail.com>
2024-12-13 16:44:27 +0900

    ルートパスのリダイレクト先をhome#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
-  root to: 'users#index'
+  root to: 'home#index'
   resources :users, only: %i(index show)
   resources :reports do
     scope module: :reports do

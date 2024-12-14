7f387a2c6cd872723592a9e956b3d9bef26d954b
akiyama <ryo.akiyama112345@gmail.com>
2024-12-14 22:30:15 +0900

    ユーザーに関するページのデザインをtailwindCSSに変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// config/tailwind.config.js   // configディレクトリ内に配置
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
+      colors: {
+        "custom-start": "#abecd6",
+        "custom-end": "#fbed96",
+      },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};

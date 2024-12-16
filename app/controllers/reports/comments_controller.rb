a5604cafcf6952cb06568357effcc69f04dc132e
akiyama <ryo.akiyama112345@gmail.com>
2024-12-16 16:02:56 +0900

    コメント機能のバリデーション処理を改善
    - コメントの必須チェックをモデルバリデーションに移行
    - 保存失敗時のエラーハンドリングを追加
    - エラーメッセージの日本語表示を改善（「コンテンツ」→「コメント」）

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -2,11 +2,15 @@
 
 class Reports::CommentsController < ApplicationController
   def create
-    report = Report.find(params[:report_id])
-    comment = report.comments.build(comment_params)
-    comment.user = current_user
-    comment.save!
-    redirect_to report, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    @report = Report.find(params[:report_id])
+    @comment = @report.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      redirect_to @report, alert: @comment.errors.full_messages.join(', ')
+    end
   end
 
   private
